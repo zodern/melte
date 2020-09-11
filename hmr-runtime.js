29810cd2e4bb62df5a75cea0a5402501341a7a62
zodern <zodern@users.noreply.github.com>
2020-09-11 10:59:48 -0500

    Fix unnecessary rerenders with HMR

--- hmr-runtime.js
+++ hmr-runtime.js
@@ -4,14 +4,25 @@ module.exports.applyHmr = makeApplyHmr(args => {
   // Mark this file as reloadable
   args.m.hot.accept();
 
+  let acceptCallback = null;
+  if (args.m.hot.data?.acceptCallback) {
+    // svelte-hmr expects accept to work as with nollup or vite
+    // applying changes is done synchronously, so we wait until after it is done
+    setTimeout(() => args.m.hot.data.acceptCallback(), 10);
+  }
+
+  args.m.hot.dispose((data) => {
+    if (acceptCallback) {
+      data.acceptCallback = acceptCallback;
+    }
+  });
+
   return Object.assign({}, args, {
     hot: {
       ...args.m.hot,
       accept(cb) {
-        // svelte-hmr expects accept to work as nollup or vite
-        // applying changes is done synchronously, so we wait until after it is done
-        setTimeout(() => cb(), 10);
+        acceptCallback = cb;
       }
     }
-  })
+  });
 });
