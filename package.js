1b8a0dbf905b1e323997b916cce5dd802fa800e7
zodern <zodern@users.noreply.github.com>
2023-01-25 13:31:56 -0600

    Update zodern:melte-compiler

--- package.js
+++ package.js
@@ -10,7 +10,7 @@ Package.registerBuildPlugin({
   name: 'melte-compiler',
   use: [
     'ecmascript@0.12.7',
-    'zodern:melte-compiler@1.3.1'
+    'zodern:melte-compiler@1.4.0'
   ],
   sources: [
     'plugin.js'
